51fe3f24404e7661ed7985e0c93ff97c0b0b96e2
Ranjit <Rnzeet>
2024-01-29 15:18:22 +0530

    code

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,14 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Alert, Linking } from 'react-native';
 
 const HomeScreen = () => {
+  const [hasReservation, setHasReservation] = useState(null);
 
- 
+  const reservationYes = () => {
+      Alert.alert("Yay!", "You have a reservation!", [{ text: "OK", onPress: () => console.log("OK Pressed") }]);
+  };
+  const reservationNo = () => {
+      Alert.alert(
+        "Oops!",
+        "You don't have a reservation. Do you want to book an Reservation?",
+        [
+          { text: "Cancel", onPress: () => console.log("Cancel Pressed") },
+          // { text: "OK", onPress: () => Linking.openURL('https://www.wbhotels.in/') },
+        ]
+      );
+  };
+
   return (
     <View style={styles.container}>
-     
-    <Text>hiiiii hello</Text>
+      <Text style={styles.text}>Do you have a reservation?</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={reservationYes}>
+          <Text style={styles.buttonText}>Yes</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={reservationNo}>
+          <Text style={styles.buttonText}>No</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -19,5 +39,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f7f7f7',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
